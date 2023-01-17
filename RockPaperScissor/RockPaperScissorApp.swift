401c031eef6f019208f2fbee434ee4aadf7ad823
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2023-01-16 19:29:52 -0800

    Initial Commit

---
+++ RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by Ikbal Demirdoven on 2023-01-16.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
